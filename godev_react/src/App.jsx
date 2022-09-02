0039cd6bf40ca0c508f2b893ee1aa87cd0edec7e
Pedro Melo <dev.pedrohenryque@gmail.com>
2022-09-02 18:35:50 -0300

    first commit

---
+++ godev_react/src/App.jsx
@@ -0,0 +1,14 @@
+import { Component, useState } from 'react'
+import Cart from './Cart'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Cart />
+      </div>
+    )
+  }
+}
+
+export default App
